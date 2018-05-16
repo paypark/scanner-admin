dab1c3a28b0a32cea918b51be2436e9f4b21b069
Ken Steiner <kenosteiner@gmail.com>
2018-05-16 15:34:22 +0200

    change commands to subprocess module due to py 3wq

--- src/services/CommandLineService.py
+++ src/services/CommandLineService.py
@@ -1,11 +1,11 @@
-import commands
+import subprocess
 
 class CommandLineService(object):
 
     @staticmethod
     def run_command(command):
         # start = time.time()
-        ret_code, output = commands.getstatusoutput(command)
+        ret_code, output = subprocess.getstatusoutput(command)
         if ret_code == 1:
             raise Exception("FAILED: %s" % command)
         # end = time.time()
